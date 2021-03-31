14b24eb746f8febfbc62dbfa8ebe27951b2bffc7
Abir Mahmud <amabirmahmud@gmail.com>
2021-03-31 18:31:50 +0600

    Page 178
    Checkout feature with form validation

--- src/app/model/model.module.ts
+++ src/app/model/model.module.ts
@@ -1,10 +1,12 @@
+import { OrderRepository } from './order.repository';
+import { Order } from './order.model';
 import { Cart } from './cart.model';
 import { NgModule } from '@angular/core';
 import { ProductRepository } from './product.repository';
 import { StaticDataSource } from './static.datasource';
 
 @NgModule({
-    providers: [ProductRepository, StaticDataSource, Cart]
+    providers: [ProductRepository, StaticDataSource, Cart, Order, OrderRepository]
 })
 export class ModelModule {
 
--- src/app/store/checkout.component.ts
+++ src/app/store/checkout.component.ts
@@ -1,11 +1,32 @@
+import { OrderRepository } from './../model/order.repository';
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Order } from '../model/order.model';
 
 
 
 @Component({
-    template: `<div><h3 class="bg-info p-1 text-white">Checkout Component</h3></div>`
+    templateUrl: 'checkout.component.html',
+    styleUrls: ['checkout.component.css']
 })
 
 export class CheckoutComponent {
 
+    orderSent = false;
+    submitted = false;
+
+    constructor(public repository: OrderRepository, public order: Order) { }
+
+    // tslint:disable-next-line: typedef
+    submitOrder(form: NgForm) {
+        this.submitted = true;
+
+        if (form.valid) {
+            this.repository.saveOrder(this.order).subscribe(order => {
+                this.order.clear();
+                this.orderSent = true;
+                this.submitted = false;
+            });
+        }
+    }
 }
