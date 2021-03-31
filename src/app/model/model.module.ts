eb74e3acb7830d004b6386b5710ad6d36c374403
Abir Mahmud <amabirmahmud@gmail.com>
2021-03-31 22:44:02 +0600

    Page 181 (Chapter 8 ended)
    Added API calling feature (Commented)

--- src/app/model/model.module.ts
+++ src/app/model/model.module.ts
@@ -1,13 +1,19 @@
+import { RestDataSource } from './rest.datasource';
 import { OrderRepository } from './order.repository';
 import { Order } from './order.model';
 import { Cart } from './cart.model';
 import { NgModule } from '@angular/core';
 import { ProductRepository } from './product.repository';
 import { StaticDataSource } from './static.datasource';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-    providers: [ProductRepository, StaticDataSource, Cart, Order, OrderRepository]
+    // for getting data from API
+    // imports: [HttpClientModule],
+    // providers: [ProductRepository, Cart, Order, OrderRepository, { provide: StaticDataSource, useClass: RestDataSource }]
+    providers: [ProductRepository, Cart, Order, OrderRepository, StaticDataSource]
 })
+
 export class ModelModule {
 
     // tslint:disable-next-line: typedef
